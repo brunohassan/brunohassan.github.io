e8f076fa7390ca9d2eba601cb847c30b23176912
Bruno Mouazzem <brunomouazzem@gmail.com>
2021-08-13 22:24:55 -0300

    add main css, js e foto

---
+++ js/script.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    $('.mobile-menu-icon').css('margin-top', $('.mobile-menu-slices').height() / 2);
+    // $('.site-main').css('margin-top', window.innerHeight - $('.main-nav').height()); // trecho para add navbar fixa no top
+});
